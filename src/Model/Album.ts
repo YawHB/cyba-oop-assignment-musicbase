4872b3608e478b96a8afd537aef989145acfc826
YawHB <yawhboateng@gmail.com>
2023-10-09 15:16:54 +0200

    Created Album class

--- src/Model/Album.ts
+++ src/Model/Album.ts
@@ -1,3 +1,23 @@
 export class Album {
+    constructor(
+        private title: string,
+        private yearOfRelease: number,
+        private image: string,
+        private id: number
+    ) {}
 
+    public getTitle(): string {
+        return this.title;
+    }
+    public getYearOfRelease(): number {
+        return this.yearOfRelease;
+    }
+
+    public getImage(): string {
+        return this.image;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
 }
