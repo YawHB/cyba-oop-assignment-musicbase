4ecdf9158c975053f4dc42cdc94fd4971ef711e1
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-10-10 23:08:42 +0200

    changed filename

---
+++ src/view/Dialog.ts
@@ -0,0 +1,37 @@
+import Artist from "../model/Artist.js";
+import Album from "../model/Album.js";
+import Track from "../model/Track.js";
+
+export default abstract class Dialog {
+    private static dialogWindow = document.querySelector(
+        "#main-dialog-frame"
+    ) as HTMLDialogElement;
+
+    protected static dialogContent = document.querySelector(
+        "#main-dialog-content"
+    ) as HTMLElement;
+
+    static open(): void {
+        this.dialogWindow.showModal();
+    }
+
+    static close(): void {
+        this.dialogWindow.close();
+    }
+
+    static clear(): void {
+        this.dialogContent.innerHTML = "";
+    }
+
+    protected abstract renderHTML(html: string): void;
+
+    protected abstract postRender(): void;
+
+    public abstract create(item: Artist | Album | Track): void
+
+    public abstract details(item: Artist | Album | Track): void
+
+    public abstract update(item: Artist | Album | Track): void
+
+    public abstract delete(item: Artist | Album | Track): void
+}
