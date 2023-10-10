efb78cc4242d03ffc84c159629f6d8e44e567e2a
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-10-10 22:58:45 +0200

    added AlbumDialog class

---
+++ src/view/AlbumDialog.ts
@@ -0,0 +1,34 @@
+import Dialog from "./Dialog.js";
+import Artist from "../model/Artist.js";
+import Album from "../model/Album.js";
+import Track from "../model/Track.js";
+
+export default class AlbumDialog extends Dialog {
+
+    protected renderHTML(html: string): void {
+        Dialog.clear()
+        Dialog.open()
+        Dialog.dialogContent.insertAdjacentHTML('beforeend', html)
+    }
+
+    protected postRender(): void {
+    }
+
+    create(item: Artist | Album | Track): void {
+    }
+
+    delete(item: Artist | Album | Track): void {
+    }
+
+    public details(item: Album): void {
+        this.renderHTML(/*html*/ `
+        <h2>${item.title}</h2>
+        <img src="${item.image}" alt="${item.title}">
+    `);
+    }
+
+    update(item: Artist | Album | Track): void {
+    }
+
+
+}
