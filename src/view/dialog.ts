046f8b9edbebd9c4dbd452328b79dc9e797bf6b1
Forkeh <forkeh@gmail.com>
2023-10-10 20:33:52 +0200

    created dialog class

---
+++ src/view/dialog.ts
@@ -0,0 +1,25 @@
+export default abstract class Dialog {
+    private static dialogWindow = document.querySelector(
+        "#main-dialog-frame"
+    ) as HTMLDialogElement;
+
+    private static dialogContent = document.querySelector(
+        "#main-dialog-content"
+    ) as HTMLElement;
+
+    static open(): void {
+        this.dialogWindow.showModal();
+    }
+
+    static close(): void {
+        this.dialogWindow.close();
+    }
+
+    static clear(): void {
+        this.dialogContent.innerHTML = "";
+    }
+
+    protected abstract renderHTML(): string;
+
+    protected abstract postRender(): void;
+}
