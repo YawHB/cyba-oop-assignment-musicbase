cea287c80204d5c67fa63c9ca6ec4a2f77835f9d
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-10-11 23:56:06 +0200

    cleanup

--- src/model/Album.ts
+++ src/model/Album.ts
@@ -1,17 +1,7 @@
 export default class Album {
     [key: string]: any;
+
     constructor(public title: string, public yearOfRelease: number, public image: string, private id: number) {}
-
-    // public getTitle(): string {
-    //     return this.title;
-    // }
-    // public getYearOfRelease(): number {
-    //     return this.yearOfRelease;
-    // }
-
-    // public getImage(): string {
-    //     return this.image;
-    // }
 
     public getId(): number {
         return this.id;
