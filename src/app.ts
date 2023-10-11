4d3313ee8ddd2934a5cdd3688ad8cbda58d82ec8
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-10-11 23:07:19 +0200

    added dropdown create button

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 "use strict";
 import DataHandler from "./components/dataHandler.js";
-import search from "./components/search.js";
 import AlbumRenderer from "./view/AlbumRenderer.js";
 import ArtistRenderer from "./view/ArtistRenderer.js";
 import ListRenderer from "./view/ListRenderer.js";
 import TrackRenderer from "./view/TrackRenderer.js";
 import Dialog from "./view/Dialog.js";
 import ArtistDialog from "./view/ArtistDialog.js";
+import AlbumDialog from "./view/AlbumDialog.js";
+import TrackDialog from "./view/TrackDialog.js";
 
 window.addEventListener("load", app);
 
@@ -28,13 +29,23 @@ async function app() {
     albumRenders.renderList();
     trackRenders.renderList();
 
-    initiateEventListeners();
+    await initiateEventListeners();
 }
 
-function initiateEventListeners() {
-    document.querySelector("#btn-close-dialog-frame")?.addEventListener("click", () => Dialog.close());
-    document.querySelector("#btn-create-artist")?.addEventListener("click", () => {
+async function initiateEventListeners() {
+    document.querySelector(".create-dropdown-btn")?.addEventListener("click", showDropdownContent);
+    window.addEventListener("click", closeDropdown);
+
+    document.querySelector("#close-dialog-frame-btn")?.addEventListener("click", () => Dialog.close());
+
+    document.querySelector("#create-artist-btn")?.addEventListener("click", () => {
         new ArtistDialog().create();
+    });
+    document.querySelector("#create-album-btn")?.addEventListener("click", () => {
+        new AlbumDialog().create();
+    });
+    document.querySelector("#create-track-btn")?.addEventListener("click", () => {
+        new TrackDialog().create();
     });
 
     const searchbar = document.querySelector("#searchbar") as HTMLInputElement;
@@ -45,6 +56,29 @@ function initiateEventListeners() {
         albumRenders.setSearchValue(searchValue);
         trackRenders.setSearchValue(searchValue);
     });
+}
+
+// show the dropdown menu if the user clicks on the dropdown button
+function showDropdownContent(event: Event) {
+    const target = event.target as HTMLElement
+    const dropdownContent = target.nextElementSibling as HTMLElement;
+
+    if (dropdownContent) {
+        dropdownContent.classList.toggle("visible");
+    }
+}
+
+// close the dropdown menu if the user clicks outside of it
+function closeDropdown(event: Event) {
+    const target = event.target as HTMLElement;
+
+    // Check if the target or its parent is the create-dropdown element
+    const isInsideDropdown = target.closest(".create-dropdown") as HTMLElement;
+
+    if (!isInsideDropdown) {
+        const dropdownContent = document.querySelector(".create-dropdown-content") as HTMLElement;
+        dropdownContent.classList.remove("visible");
+    }
 }
 
 export { artistRenders, albumRenders, trackRenders };
