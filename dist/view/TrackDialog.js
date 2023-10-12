9e2341519f08f76dad647a0108e213c20feee3f1
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-10-12 10:31:02 +0200

    added details, delete amd postRender methods

--- dist/view/TrackDialog.js
+++ dist/view/TrackDialog.js
@@ -1,22 +1,71 @@
 import Dialog from "./Dialog.js";
+import DataHandler from "../components/dataHandler.js";
+import { trackRenders } from "../app.js";
 export default class TrackDialog extends Dialog {
-    renderHTML(html) {
+    async renderHTML(html) {
         Dialog.clear();
         Dialog.open();
         Dialog.dialogContent.insertAdjacentHTML('beforeend', html);
     }
-    postRender() {
+    async postRender(item) {
+        try {
+            const updateButton = document.querySelector(".track-dialog-update-button");
+            const deleteButton = document.querySelector(".track-dialog-delete-button");
+            if (!updateButton || !deleteButton) {
+                throw new Error("No buttons found");
+            }
+            updateButton.addEventListener("click", () => {
+                this.update(item);
+            });
+            deleteButton.addEventListener("click", () => {
+                this.delete(item);
+            });
+        }
+        catch (error) {
+            console.error(error.message);
+        }
     }
-    create() {
+    async create() {
         console.log("create track");
     }
-    delete(item) {
+    async delete(item) {
+        try {
+            await DataHandler.deleteData("tracks", item.getId());
+            const index = DataHandler.tracksArr.indexOf(item);
+            DataHandler.tracksArr.splice(index, 1);
+            Dialog.close();
+            trackRenders.setList(DataHandler.tracksArr);
+            trackRenders.clearList();
+            trackRenders.renderList();
+        }
+        catch (error) {
+            console.error(error.message);
+        }
     }
-    details(item) {
-        const html = `
-        <h2>${item.title}</h2>`;
-        this.renderHTML(html);
+    async details(item) {
+        try {
+            const html = `
+                <article class="track-details">
+                <h2>${item.title}</h2>
+                <h3>Track Details</h3>
+                <p>Artist: ${item.artists}</p>
+                <p>Album: ${item.albums}</p>
+                <p>Duration: ${item.getDuration()}</p>
+                
+                <div class="track-dialog-buttons">
+                    <button class="track-dialog-update-button">Update</button>
+                    <button class="track-dialog-delete-button">Delete</button>
+                </div>
+                </article>
+                `;
+            await this.renderHTML(html);
+            await this.postRender(item);
+        }
+        catch (error) {
+            console.error(error.message);
+        }
     }
-    update(item) {
+    async update(item) {
+        console.log("update track");
     }
 }
--- src/view/TrackDialog.ts
+++ src/view/TrackDialog.ts
@@ -1,35 +1,85 @@
 import Dialog from "./Dialog.js";
-import Artist from "../model/Artist.js";
-import Album from "../model/Album.js";
 import Track from "../model/Track.js";
+import DataHandler from "../components/dataHandler.js";
+import {trackRenders} from "../app.js";
 
 export default class TrackDialog extends Dialog {
 
-    protected renderHTML( html: string): void {
+    protected async renderHTML( html: string): Promise<void> {
         Dialog.clear()
         Dialog.open()
         Dialog.dialogContent.insertAdjacentHTML('beforeend', html)
     }
 
-    protected postRender(): void {
+    protected async postRender(item: Track): Promise<void> {
+        try {
+
+            const updateButton = document.querySelector(".track-dialog-update-button") as HTMLButtonElement;
+            const deleteButton = document.querySelector(".track-dialog-delete-button") as HTMLButtonElement;
+
+            if (!updateButton || !deleteButton) {
+                throw new Error("No buttons found");
+            }
+
+            updateButton.addEventListener("click", () => {
+                this.update(item);
+            });
+            deleteButton.addEventListener("click", () => {
+                this.delete(item);
+            });
+            } catch (error) {
+            console.error((error as Error).message);
+        }
     }
 
-    create(): void {
+    async create(): Promise<void> {
         console.log("create track")
     }
 
-    delete(item: Artist | Album | Track): void {
+    async delete(item: Track): Promise<void> {
+        try {
+            await DataHandler.deleteData("tracks", item.getId());
+            const index = DataHandler.tracksArr.indexOf(item);
+            DataHandler.tracksArr.splice(index, 1);
+            Dialog.close();
+            // render tracks list again
+            trackRenders.setList(DataHandler.tracksArr);
+            trackRenders.clearList();
+            trackRenders.renderList();
+
+        } catch (error) {
+            console.error((error as Error).message);
+        }
     }
 
-    public details(item: Track): void {
+    public async details(item: Track): Promise<void> {
+        try {
+            const html = /*html*/ `
+                <article class="track-details">
+                <h2>${item.title}</h2>
+                <h3>Track Details</h3>
+                <p>Artist: ${item.artists}</p>
+                <p>Album: ${item.albums}</p>
+                <p>Duration: ${item.getDuration()}</p>
+                
+                <div class="track-dialog-buttons">
+                    <button class="track-dialog-update-button">Update</button>
+                    <button class="track-dialog-delete-button">Delete</button>
+                </div>
+                </article>
+                `;
 
-        const html = /*html*/ `
-        <h2>${item.title}</h2>`;
-        
-        this.renderHTML(html);
+            await this.renderHTML(html);
+            await this.postRender(item);
+
+
+        } catch (error) {
+            console.error((error as Error).message);
+        }
     }
 
-    update(item: Artist | Album | Track): void {
+    async update(item: Track): Promise<void> {
+        console.log("update track")
     }
 
 
